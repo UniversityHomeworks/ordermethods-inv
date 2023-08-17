576c68c36a6f33e023dd50adb5ab2c5edceffea5
jonakls <jonakls@JONAKLS-PC>
2023-08-17 00:04:14 -0500

    Agregar archivos de proyecto.

---
+++ OrderMethods-Inv/Api/OrderMethod.cs
@@ -0,0 +1,22 @@
+﻿namespace OrderMethods_Inv.Api
+{
+    public interface OrderMethod
+    {
+        void AddElement(int element);
+
+        void RemoveElement(int element);
+
+        void RemoveElementOfPosition(int position);
+
+        void ShuffleElements();
+
+        void Clear();
+
+        void RandomElements(int size);
+
+        void RandomElements()
+        {
+            this.RandomElements(5);
+        }
+    }
+}
